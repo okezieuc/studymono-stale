7b585f14c27746416bcf8f6a13133fe4b0de5865
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-02-27 17:43:17 +0100

    I made fallback to be false
    To deploy on cloudflare pages, all `fallbacks` must be set to false

--- src/pages/pastquestions/[...questionid].js
+++ src/pages/pastquestions/[...questionid].js
@@ -108,7 +108,7 @@ export async function getStaticPaths() {
 		current++
 	}
 	
-  return { paths, fallback: true }
+  return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }) {
