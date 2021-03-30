72871576fd18ef25e628df991b493229769ccb79
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-03-30 19:15:55 +0100

    Fixed the layout on the privacy page

--- src/components/pages/PageHeading.js
+++ src/components/pages/PageHeading.js
@@ -1,7 +1,7 @@
 import { Box, Heading, Text, } from '@chakra-ui/react'
 
 export const PageHeading = () => (
-    <Box m={[8, null, 20]} py={[12, null, 20]} textAlign="center" bg="white" boxShadow="0px 0px 2px 0px">
+    <Box m={[4, null, 20]} py={[12, null, 20]} textAlign="center" bg="white" boxShadow="0px 0px 2px 0px">
         <Heading as="h1" fontSize={["2xl", null, "4xl"]}>
             Data we Collect
         </Heading>
--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -1,10 +1,8 @@
 import {
-  Link as ChakraLink,
-	Box, Heading, Text,
+ 	Box, Text,
+	Container,
 	
 } from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import Link from 'next/link'
 import { NavBar, } from '../components/NavBar'
 import { Footer, } from '../components/Footer'
 import { CallToAction, } from '../components/CallToAction'
@@ -15,17 +13,23 @@ import { getRecommendedPosts } from '../lib/api'
 const Index = ({ recommendedPosts }) => (
   <Box>
 	 	<NavBar />
+		<Box bg="gray.500" h={[6, null, 8]} />
+		<Box bg="gray.400" h={[40, null, 56]} mb={[-28, null -48]} />
 		<PageHeading />
-		<Heading fontSize="6xl">
-			Privacy
-		</Heading>
-		<Text p="4">
+		
+		<Container maxW="5xl" mb={[20, null, 40]}>
+		<Text>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae sodales ex, vel consequat sem. Donec quis faucibus mauris. Vivamus condimentum bibendum felis id egestas. Mauris convallis ipsum ac ligula ultricies tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non turpis ultrices, tincidunt magna id, semper lectus. Sed vel ligula a dolor finibus lobortis. Etiam viverra tempus auctor. Phasellus congue quis neque ut commodo. Sed ultrices augue et lorem vestibulum maximus. Fusce ac velit eget nisi rhoncus feugiat et vel magna. Quisque luctus augue nec sem facilisis gravida. Integer lacus nisi, varius et scelerisque a, luctus at purus. Etiam feugiat tellus eu elit ultricies posuere. Vestibulum pulvinar metus vel sollicitudin mattis.
-
 		</Text>
-		<Link href="/">
-			<a>Return to homepage</a>
-		</Link>
+		<br />
+		<Text>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae sodales ex, vel consequat sem. Donec quis faucibus mauris. Vivamus condimentum bibendum felis id egestas. Mauris convallis ipsum ac ligula ultricies tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non turpis ultrices, tincidunt magna id, semper lectus. Sed vel ligula a dolor finibus lobortis. Etiam viverra tempus auctor. Phasellus congue quis neque ut commodo. Sed ultrices augue et lorem vestibulum maximus. Fusce ac velit eget nisi rhoncus feugiat et vel magna. Quisque luctus augue nec sem facilisis gravida. Integer lacus nisi, varius et scelerisque a, luctus at purus. Etiam feugiat tellus eu elit ultricies posuere. Vestibulum pulvinar metus vel sollicitudin mattis.
+		</Text>
+		<br />
+		<Text>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae sodales ex, vel consequat sem. Donec quis faucibus mauris. Vivamus condimentum bibendum felis id egestas. Mauris convallis ipsum ac ligula ultricies tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non turpis ultrices, tincidunt magna id, semper lectus. Sed vel ligula a dolor finibus lobortis. Etiam viverra tempus auctor. Phasellus congue quis neque ut commodo. Sed ultrices augue et lorem vestibulum maximus. Fusce ac velit eget nisi rhoncus feugiat et vel magna. Quisque luctus augue nec sem facilisis gravida. Integer lacus nisi, varius et scelerisque a, luctus at purus. Etiam feugiat tellus eu elit ultricies posuere. Vestibulum pulvinar metus vel sollicitudin mattis.
+		</Text>
+		</Container>
 		<RecommendedReads posts={recommendedPosts} />
 		<CallToAction />
 		<Footer hideTop={true} />
