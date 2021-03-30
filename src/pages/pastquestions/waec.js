2936e379a9873dce61f57c1e9ebc973a9ce957d4
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-03-30 21:01:54 +0100

    Fixed the layout on the WAEC and JAMB pages

--- src/pages/pastquestions/waec.js
+++ src/pages/pastquestions/waec.js
@@ -1,6 +1,6 @@
 import {
   Link as ChakraLink,
-	Box, Heading, Text,
+	Box, Heading, Text, Container,
 	SimpleGrid,
 } from '@chakra-ui/react'
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
@@ -15,30 +15,37 @@ import { getRecommendedPosts } from '../../lib/api'
 const Index = ({ subjects, recommendedPosts }) => (
   <Box>
 	  	<NavBar />
-		<Box m={[4, null, 20]}>
-			<Heading fontSize={["4xl", null, "6xl"]} mb={[4, null, 8]}>
-				WAEC
-			</Heading>
-			<ExamPageInfo />
+
+		  <Box bg="gray.200" pt={[12, null, 20]} pb={[20, null, 40]}>
+			<Container maxW="5xl" >
+				<Heading fontSize={["4xl", null, "6xl"]} mb={[4, null, 8]}>
+					WAEC
+				</Heading>
+				<ExamPageInfo />
+			</Container>
 		</Box>
 
-		<Heading as="h3" mx={[4, null,20]} mb={4} fontSize={["lg", null, "3xl"]} mt={[12, null, 24]}>Subjects</Heading>
-		<SimpleGrid columns={[2, null, 2]} gap={[4, null, 12]} mx={[4, null,20]}>
+		<Container maxW="5xl" mb={[20, null, 40]} >
+		<Heading as="h3" mb={4} fontSize={["lg", null, "3xl"]} mt={[12, null, 24]}>Subjects</Heading>
+		<SimpleGrid columns={[2, null, 3]} gap={[4, null, 12]}>
 			<SubjectCard subject="Physics" slug="/pastquestions/waec/phy/1" />
-			<SubjectCard subject="Chemistry" slug="/pastquestions/waec/chem/1" />
+			<SubjectCard subject="Chemistry" slug="/pastquestions/waec/chem/1" />			
+			<SubjectCard subject="Biology" slug="/pastquestions/waec/bio/1"
+				gridColumn={["span 2", null, "span 1"]} />			
 		</SimpleGrid>
-		<SimpleGrid columns={[3, null, 3]} gap={[4, null, 12]} mt={[4, null, 12]} mx={[4, null,20]}
+		<SimpleGrid columns={[2, null, 4]} gap={[4, null, 12]} mt={[4, null, 12]}
 			mb={[8, null, 28]}>
 			{
 				Object.keys(subjects).map((key) => (<>
 					{
-						(key != "PHY" && key != "CHEM") ? <SubjectCard subject={subjects[key]} key={key}
+						(key != "PHY" && key != "CHEM" && key != "BIO") ? <SubjectCard subject={subjects[key]} 
 							slug={`/pastquestions/waec/${key.toLowerCase()}/1`} subtitle="Sample Subtitle" /> : ''
 					}
 					</>)
 				)
 			}
 		</SimpleGrid>
+		</Container>
 		
 		
 		<RecommendedReads posts={recommendedPosts} />
