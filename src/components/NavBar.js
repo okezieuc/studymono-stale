b1aebeec5f61e31fba48c3afc11967ca2472113b
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-03-31 14:39:36 +0100

    Improved the styling of the NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,21 +6,23 @@ export const NavBar = () => <Box>
 <Box fontSize={["sm", "sm", "md"]} bg="black" w="100%" p="2" pl={["2", null, "6"]} color="white">
 	Special announcement that is displayed at top
 </Box>
-<Flex fontSize={["sm", "sm", "md"]} pl={["1", null, "4"]} bg="white" borderBottom="1px solid">
+<Box fontSize={["sm", "sm", "md"]} pl={["1", null, "4"]} bg="white" borderBottom="1px solid">
 	<Box p={["1", null, "2"]}>
 		<HStack spacing={["4", null, "12"]}>
-			<Box bg="yellow.2000" fontSize="" fontWeight="black" mr={["1", null, "4"]}>
+			<Box bg="yellow.2000" fontSize="2xl" fontWeight="black" mr={["1", null, "4"]}>
 				<Link href="/">
 					studymono.
 				</Link>
 			</Box>
 			<Link href="/pastquestions">Past Questions</Link>
 			<Link href="/blog">Blog</Link>
+			<Spacer />
+			<Box d={["none", null, "block"]} bg="white" py={["1", null, "2"]} px={["1", null, "6"]} fontWeight="bold" color="#4715be"
+				border="#4715be 1px solid" borderRadius={8} >
+				<Link href="/pastquestions">Get Started</Link>
+			</Box>
 		</HStack>
 	</Box>
-	<Spacer />
-	<Box d={["none", null, "block"]} bg="#4715be" py={["1", null, "2"]} px={["1", null, "6"]} fontWeight="bold" color="white">
-		<Link href="/pastquestions">Get Started</Link>
-	</Box>
-</Flex>
+	
+</Box>
 </Box>
