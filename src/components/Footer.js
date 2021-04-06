5b2dcef439a1986dbb40a05516b91e4bf6a8acc4
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-04-06 23:02:26 +0100

    Worked on visual improvements to the navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ import Link from 'next/link'
 
 export const Footer = ({hideTop = false}) => <Box>
 	
-	<SimpleGrid columns={[2, null, 4]} spacingX={[4, null,10]} spacingY={[6, null,10]}
+	<SimpleGrid columns={[2, null, 6]} spacingX={[4, null,10]} spacingY={[6, null,10]}
 		px={[4, null, 12]} py={[8, null, 20]} borderBottom="1px solid"
 		borderTop={hideTop ? "none" : "1px solid"} color="tint.600" fontWeight="semibold"
 		fontSize={["sm", null, "lg"]} borderColor="tint.400" pb={[20, null, 20]}>
-		<Box gridColumn={["span 2", null, "span 1"]}>
+		<Box gridColumn={["span 2", null, "span 2"]}>
 			<Text fontSize={["xl", null, "3xl"]} fontWeight="bold" mb={["1", null, "4"]} color="black">studymono.</Text>
 			<Text fontSize={["sm", null, "md"]} fontWeight="semibold"
 				color="tint.500">Our simple but exquisite tag  line will be displayed below our  name</Text>
@@ -25,10 +25,22 @@ export const Footer = ({hideTop = false}) => <Box>
 		</Box>
 		<Box>
 			<Text fontSize={["sm", null, "lg"]} fontWeight="bold" mb={["1", null, "4"]} color="tint.500">How we help</Text>
-			<Link href="/blog/hello-world">Why we built this</Link>
+			<Link href="/blog/hello-world">Why we built another app</Link><br />
+			<Link href="/blog/preview">Previewing our app</Link> <br /> 
+			<Link href="/blog/dynamic-routing">Dynamic routing</Link> <br />
+		</Box>
+		<Box>
+			<Text fontSize={["sm", null, "lg"]} fontWeight="bold" mb={["1", null, "4"]} color="tint.500">Categories</Text>
+			<Link href="/pastquestions/waec">WAEC</Link><br />
+			<Link href="/pastquestions/jamb">JAMB</Link><br />
+			<Link href="/pastquestions/subject/math/1">Mathematics</Link><br />
+			<Link href="/pastquestions/subject/phy/1">Physics</Link><br />
+			<Link href="/pastquestions/subject/chem/1">Chemistry</Link><br />
+			<Link href="/pastquestions/subject/bio/1">Biology</Link><br />
+			<Link href="/pastquestions/subject/eng/1">English</Link><br />
 		</Box>
 	</SimpleGrid>
-	<Box>
+	<Box d="none">
 		<Text m="0 auto" pt="12" textAlign="center" fontSize={["md", null, "3xl"]}
 			fontWeight="bold" maxW={["250px", "500px"]}>Subscribe to our newsletter  for updates</Text>
 		<Box w={["50%", null, "30%"]} h={["30px", null, "50px"]} bg="gray.100" m="0 auto" mt="8" mb="12"></Box>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,51 @@
-import { Box, Flex, Spacer, HStack } from '@chakra-ui/react'
+import { Box, Flex, Spacer, HStack, VStack,
+	Button, useDisclosure, } from '@chakra-ui/react'
 import Link from 'next/link'
 
 
-export const NavBar = () => <Box>
+export const NavBar = () => {
+	const { isOpen, onOpen, onClose, onToggle } = useDisclosure()
+	
+	return(<Box>
 <Box fontSize={["sm", "sm", "sm"]} bg="black" w="100%" p="2" pl={["2", null, "6"]} color="white">
 	Special announcement that is displayed at top
-</Box>
-<Box fontSize={["sm", "sm", "md"]} pl={["1", null, "4"]} bg="white" borderBottom="1px solid" borderBottomColor="tint.400">
-	<Box p={["1", null, "2"]}>
+</Box> 
+<Box fontSize={["sm", "sm", "lg"]} pl={["1", null, "4"]} bg="white" borderBottom="1px solid" borderBottomColor="tint.400">
+	<Box p={["2", null, "4"]}>
 		<HStack spacing={["4", null, "12"]}>
 			<Box color="black" fontSize={["lg", null, "2xl"]} fontWeight="black" mr={["1", null, "4"]}>
 				<Link href="/">
 					studymono.
 				</Link>
 			</Box>
-			<Link href="/pastquestions">Past Questions</Link>
-			<Link href="/blog">Blog</Link>
+			<Box d={["none",null, "block"]}>
+				<Link href="/pastquestions">Past Questions</Link>
+			</Box>
+			<Box d={["none",null, "block"]}>
+				<Link href="/blog">Blog</Link>
+			</Box>
 			<Spacer />
-			<Box d={["none", null, "block"]} bg="white" py={["1", null, "2"]} px={["1", null, "6"]} fontWeight="bold" color="black"
+			<Box d={["block"]} bg="white" py={["1", null, "2"]} px={["2", null, "6"]} fontWeight="bold" color="black"
 				border="black 1px solid" borderRadius={8} >
 				<Link href="/pastquestions">Get Started</Link>
+				
 			</Box>
+			<Button size="sm" onClick={onToggle} d={["block", null, "none"]}>
+					☰
+				</Button>
 		</HStack>
+		<Box d={isOpen ? "block" : "none"}>
+			<VStack position="absolute" top="80px" left="0"
+				 bg="white" w="100%"
+				alignItems="flex-start"
+				fontSize="lg" fontWeight="bold" p={8} textAlign="left">
+				<Link href="/pastquestions">Home</Link>
+				<Link href="/pastquestions">Past Questions</Link>
+				<Link href="/pastquestions">Blog</Link>
+				<Button colorScheme="red" mt={20} size="sm" onClick={onClose}>X</Button>
+			</VStack>
+		</Box>
 	</Box>
 	
 </Box>
-</Box>
+</Box>)}
