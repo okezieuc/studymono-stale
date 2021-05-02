8c98a3a2b4ef31a1bd97c8685c3ecc845bf10bff
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-05-02 23:23:03 +0100

    Fix texts to use correct fonts

--- src/components/landing/Stats.js
+++ src/components/landing/Stats.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export const Stats = () => (<Box maxW={["xl",null, "5xl"]} m="0 auto" textAlign={["center", null, "left"]} px={[4, null, 0]}>
 	<Stack direction={["column", null, "row"]} align="center" mt={[24,null]}>
 		<Box flex="1" mr={[null, null, 12]}>
-			<Text fontSize={["4xl", null, "5xl"]} fontWeight="bold">Studymono is for you!</Text>
+			<Heading fontSize={["4xl", null, "5xl"]} fontWeight="bold" as="h2">Studymono is for you!</Heading>
 			<Box w={["200px", null, "300px"]} h={["200px", null, "300px"]} d={["block", null, "none"]} m="0 auto">
 				<Logo src="/logo.svg" size="xs" m="0 auto" />
 			</Box>
