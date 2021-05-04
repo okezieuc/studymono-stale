a2bedda4a70a98b40a806b95c8c0c7d56907821e
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-05-04 17:36:50 +0100

    Add favicon images

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,11 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+					<link rel="shortcut icon" href="/favicon.png" />
+					<link rel="icon" type="image/png" href="/favicon-196x196.png" />
+					<link rel="apple-touch-icon" href="/touch-icon-iphone.png" />
+				</Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
