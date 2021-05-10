be3e83f06a830ec9e7a0a33cc187c23f62d0786b
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-05-10 07:45:01 +0100

    Inline white SVG logo

--- src/components/landing/Vision.js
+++ src/components/landing/Vision.js
@@ -10,6 +10,7 @@ export const Vision = () => (<Box mx={[4]} >
 			<Logo 
 				size="xs"
 				alt="Studymono's logo"
+				src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI4MDAiIGhlaWdodD0iODAwIiB2aWV3Qm94PSIwIDAgMjExLjY2NyAyMTEuNjY3IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmYiIHN0cm9rZS13aWR0aD0iOCIgeG1sbnM6dj0iaHR0cHM6Ly92ZWN0YS5pby9uYW5vIj48cmVjdCB3aWR0aD0iMTU1LjgwMyIgaGVpZ2h0PSIxNTUuODAzIiB4PSI0IiB5PSI0IiByeD0iMTYiIHBhaW50LW9yZGVyPSJub3JtYWwiLz48cmVjdCB5PSI1MS44NjQiIHg9IjUxLjg2NCIgaGVpZ2h0PSIxNTUuODAzIiB3aWR0aD0iMTU1LjgwMyIgcng9IjE2IiBwYWludC1vcmRlcj0ibm9ybWFsIi8+PC9zdmc+"
 			/>
 		</Box>
 		<Box flex="1">
