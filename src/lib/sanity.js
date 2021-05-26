2da8658882d02130a7acffad3a043d715ed54815
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-05-27 00:45:48 +0100

    Use token to access Sanity API

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -4,7 +4,8 @@ export default sanityClient({
   // Find your project ID and dataset in `sanity.json` in your studio project
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
-  useCdn: false
+  useCdn: false,
+	token: process.env.SANITY_TOKEN
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
   // data always (potentially slightly slower and a bit more expensive).
