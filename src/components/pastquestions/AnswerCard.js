4e9176b710de36a700c3d296082344f1a1c29966
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-06-05 19:24:10 +0100

    Add spacing between paragraphs in explanation and change background color to white.

--- src/components/pastquestions/AnswerCard.js
+++ src/components/pastquestions/AnswerCard.js
@@ -2,6 +2,7 @@ import { Box, } from '@chakra-ui/react'
 import BlockContent from "./BlockContent";
 
 //const sampleexplanation="Ohm's law is a law about the conservation of electricity. Ohms law states that the sum of the angles in a right angled triangle is the same as the sum of the angles in a normal equilateral triangle which is 180 degrees. Based on this, we can conclude that the correct answer for this question can neither be B, C nor D. Therefore, the correct answer is option A."
+
 export const AnswerCard = ({answer, explanation}) => (<>
 	<Box 
 		bg="brand"
@@ -19,9 +20,11 @@ export const AnswerCard = ({answer, explanation}) => (<>
 		? <>
 			<Box 
 				p={[6,null, 12]}
+				background="white"
 				fontSize={["sm", "lg", "xl"]}
 				border="1px solid"
 				borderColor="brand"
+				className="paragraph-space"
 			>
 				<BlockContent
 					body={explanation}
