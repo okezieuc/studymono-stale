eec72d3d36bdc20e950a5d41beaae3ad0df9dadf
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-06-06 19:47:32 +0100

    Update meta tags
    * Add explanation to the meta description if it is available.
    * Remove the exam and subject text from the meta title.
    * Add —Studymono at the end of meta title

--- src/pages/pastquestions/[...questionid].js
+++ src/pages/pastquestions/[...questionid].js
@@ -49,10 +49,14 @@ const Index = ({ questiondata, otherdata, questionid, dataschema, recommendedPos
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css" integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc" crossorigin="anonymous"/>
 			
 			
-			<title>{`${dataschema.exams[questiondata.exam.toUpperCase()]} ${dataschema.subjects[questiondata.subject.toUpperCase()]} - ${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""}`.slice(0, 120) } </title>
-			<meta property="og:title" content={`${dataschema.exams[questiondata.exam.toUpperCase()]} ${dataschema.subjects[questiondata.subject.toUpperCase()]} - ${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""}`.slice(0, 120) } />
-			<meta name="description" content={`${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""} a. ${blockToText(questiondata.optiona)} b. ${blockToText(questiondata.optionb)} c. ${blockToText(questiondata.optionc)} d. ${blockToText(questiondata.optiond)}`.slice(0,160)} />
-			<meta property="og:description" content={`${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""} a. ${blockToText(questiondata.optiona)} b. ${blockToText(questiondata.optionb)} c. ${blockToText(questiondata.optionc)} d. ${blockToText(questiondata.optiond)}`.slice(0,160)} />			
+			<title>{`${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""}—Studymono`.slice(0, 120) } </title>
+			<meta property="og:title" content={`${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""}—Studymono`.slice(0, 120) } />
+			<meta name="description" content={questiondata.explanation  
+				? `${blockToText(questiondata.explanation).slice(0, 160)}`
+				: `${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""} a. ${blockToText(questiondata.optiona)} b. ${blockToText(questiondata.optionb)} c. ${blockToText(questiondata.optionc)} d. ${blockToText(questiondata.optiond)}`.slice(0,160)} />
+			<meta property="og:description" content={questiondata.explanation  
+				? `${blockToText(questiondata.explanation).slice(0, 160)}`
+				: `${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""} a. ${blockToText(questiondata.optiona)} b. ${blockToText(questiondata.optionb)} c. ${blockToText(questiondata.optionc)} d. ${blockToText(questiondata.optiond)}`.slice(0,160)} />			
 			<meta name="og:image" content="https://res.cloudinary.com/okezieuc/image/upload/studymono/covers/past-questions-cover-image_z13zcz.png" />
 			<link rel="canonical" href={`https://www.studymono.com/pastquestions/${questionid}`} />
 			{/*
@@ -62,8 +66,10 @@ const Index = ({ questiondata, otherdata, questionid, dataschema, recommendedPos
 			<link rel="canonical" href={`https://www.studymono.com/pastquestions/${questionid}`} />
 			*/}
 			
-			<meta name="twitter:title" content={`${dataschema.exams[questiondata.exam.toUpperCase()]} ${dataschema.subjects[questiondata.subject.toUpperCase()]} - ${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""}`.slice(0, 120) } />
-			<meta name="twitter:description" content={`${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""} a. ${blockToText(questiondata.optiona)} b. ${blockToText(questiondata.optionb)} c. ${blockToText(questiondata.optionc)} d. ${blockToText(questiondata.optiond)}`.slice(0,160)} />
+			<meta name="twitter:title" content={`${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""}—Studymono`.slice(0, 120) } />
+			<meta name="twitter:description" content={questiondata.explanation  
+				? `${blockToText(questiondata.explanation).slice(0, 160)}`
+				: `${blockToText(questiondata.question)} ${!!questiondata.prompt ? (questiondata.prompt) : ""} a. ${blockToText(questiondata.optiona)} b. ${blockToText(questiondata.optionb)} c. ${blockToText(questiondata.optionc)} d. ${blockToText(questiondata.optiond)}`.slice(0,160)} />
 			<meta name="twitter:image" content="https://res.cloudinary.com/okezieuc/image/upload/studymono/covers/past-questions-cover-image_z13zcz.png" />
 			
 			<script type="application/ld+json" 
