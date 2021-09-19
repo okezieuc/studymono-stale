412e69cb0729dd0ad82d62226a4a9c78559367a4
okezieuc <53785400+okezieuc@users.noreply.github.com>
2021-09-19 16:35:26 +0100

    remove question pages from sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,29 +1,14 @@
 const fs = require('fs')
 const prettier = require('prettier')
-const sanityClient = require('@sanity/client')
 const dotenv = require('dotenv')
 const path = require('path')
 dotenv.config()
 
-const sanity = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  useCdn: false,
-	token: process.env.SANITY_TOKEN
-})
-
 const siteUrl = "https://www.studymono.com"
 
-const getLastMod = (date) => date.slice(0, 10)
-
 let locations = [
-	"", "/blog", "/terms-and-conditions",
-	"/pastquestions", "/pastquestions/waec", "/pastquestions/jamb", 
+	"", "/blog", "/terms-and-conditions", 
 ]
-
-let questionLocations = []
-
-const dataschema = {"exams":{"WAEC":"WAEC","JAMB":"JAMB"},"subjects":{"PHY":"Physics","CHEM":"Chemistry","BIO":"Biology","MATH":"Mathematics","ENG":"English"}}
 
 ;(async () => {
 	const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
@@ -36,36 +21,6 @@ const dataschema = {"exams":{"WAEC":"WAEC","JAMB":"JAMB"},"subjects":{"PHY":"Phy
 		locations.push(`/blog/${postUrl}`)
 	})
 	
-	//create /pastquestions/subject/[subject]/[number] pages 
-	for (let num=0; num<Object.keys(dataschema.subjects).length; num++) {
-		const subject = Object.keys(dataschema.subjects)[num]
-		for(let i=0; i<2; i++) {
-			locations.push(`/pastquestions/subject/${subject.toLowerCase()}/${i+1}`)
-		}
-	}
-	
-	//create /pastquestions/[exam]/[subject]/[number] pages 
-	for(let num=0; num<Object.keys(dataschema.exams).length; num++) {
-		const exam = Object.keys(dataschema.exams)[num]
-		for (let subnum=0; subnum<Object.keys(dataschema.subjects).length; subnum++) {
-			const subject = Object.keys(dataschema.subjects)[subnum]
-			for(let i=0; i<2; i++) {
-				locations.push(`/pastquestions/${exam.toLowerCase()}/${subject.toLowerCase()}/${i+1}`)
-			}
-		}
-	}
-	
-	//create /pastquestions/[...questionid] pages 
-	const questionQuery = () => `*[_type == "pastquestion" && !(_id in path('drafts.**'))]{
-		_id,
-		_updatedAt
-	}`
-	const questionList = await sanity.fetch(questionQuery())
-	questionList.map((question) => questionLocations.push({
-		url: `/pastquestions/${question._id}`,
-		lastmod: getLastMod(question._updatedAt)
-	}))
-	
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -73,13 +28,6 @@ const dataschema = {"exams":{"WAEC":"WAEC","JAMB":"JAMB"},"subjects":{"PHY":"Phy
 						locations.map((loc)=>(`
 						<url>
 							<loc>${`${siteUrl}${loc}`}</loc>
-							</url>`))
-						.join('')
-					}${
-						questionLocations.map((question)=>(`
-						<url>
-							<loc>${`${siteUrl}${question.url}`}</loc>
-							<lastmod>${`${question.lastmod}`}</lastmod>
 							</url>`))
 						.join('')
 					}
